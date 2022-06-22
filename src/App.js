d0211ae4d5469a79a1d58290c1c83490afc7e2d4
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-06-22 12:26:54 +0200

    Added basic css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="Container">
-        <h1> Weather App</h1>
+        <Weather />
         <footer>
           Open-sourced code by Sara SM on {""}
           <a
