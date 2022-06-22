44902db759feb772b08b73f18f5a18d1a8380d99
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-06-22 10:35:43 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        Open-sourced code by Sara SM on {""}
-        <a
-          href="https://github.com/SaraSaria/react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="Container">
+        <h1> Weather App</h1>
+        <footer>
+          Open-sourced code by Sara SM on {""}
+          <a
+            href="https://github.com/SaraSaria/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
