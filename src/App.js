697248b36a0bbe60f41b98518a4ace0cff29552d
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-06-22 09:53:21 +0200

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        Open-sourced code by Sara SM on {""}
+        <a
+          href="https://github.com/SaraSaria/react-weather-app"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
