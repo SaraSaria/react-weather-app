9e61c5efac02a3313ca9ff4a3dee825702d4ed2d
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-06-22 16:41:01 +0200

    added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -71,8 +71,10 @@ export default function Weather() {
     );
   } else {
     let apiKey = "02b4660409b775551d2476caecada88b";
-    let city = "Madrid";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
   }
 }
