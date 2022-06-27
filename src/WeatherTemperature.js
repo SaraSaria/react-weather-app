6048cd96b28ed6b6feeaff7f7f95b654bd5b087b
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-06-27 10:57:43 +0200

    updated weather temperature

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -22,7 +22,7 @@ export default function WeatherTemperature(props) {
         <span className="Unit">
           °C |{" "}
           <a href="/" onClick={showFahrenheit}>
-            ºF
+            º
           </a>
         </span>
       </div>
