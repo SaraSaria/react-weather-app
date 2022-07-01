adcd4216d80a3181dae8276f59ed110c3c1f8a61
SaraSaria <102535689+SaraSaria@users.noreply.github.com>
2022-07-01 09:52:22 +0200

    added weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -61,6 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
---
+++ src/WeatherForecast.js
@@ -0,0 +1,22 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <div className="WeatherForecast-icon">
+            <WeatherIcon code="01d" size={32} />
+          </div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19º</span>
+            <span className="WeatherForecast-temperature-min">10º</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
